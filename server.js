f8cc8e369d5709d5dcd2bed433f38cf54ee2801f
ReisLucasF <reislucasf@icloud.com>
2024-03-23 09:27:58 -0300

    sss

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const puppeteer = require('puppeteer');
+const axios = require('axios');
 
-const data = {
+const data = 
+// JSON.parse(fs.readFileSync('bairros.json', 'utf-8'));
+
+{
     "AL": {
         "Arapiraca": [
             "Alto do Cruzeiro",
@@ -15807,7 +15812,6 @@ const data = {
     }
 }
 
-// tt
 app.get('/:uf/:cidade', (req, res) => {
     const { uf, cidade } = req.params;
 
@@ -15821,6 +15825,70 @@ app.get('/:uf/:cidade', (req, res) => {
         res.status(404).json({ error: `UF '${uf}' não encontrada` });
     }
 });
+
+
+
+app.get('/att/:estado/:cidade', async (req, res) => {
+    const { estado, cidade } = req.params;
+
+    try {
+        const codigoMunicipio = await getCodigoMunicipio(estado, cidade);
+        const nomesDasCidades = await extrairBairros(codigoMunicipio);
+
+        res.json(nomesDasCidades);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+async function getCodigoMunicipio(estado, cidade) {
+    try {
+        const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/municipios`);
+
+        const municipio = response.data.find(municipio => municipio.nome.toUpperCase() === cidade.toUpperCase());
+
+        if (!municipio) {
+            throw new Error(`Cidade "${cidade}" do estado "${estado}" não encontrada.`);
+        }
+
+        return municipio.id;
+    } catch (error) {
+        throw new Error('Erro ao obter código do município:', error.message);
+    }
+}
+
+async function extrairBairros(codigoMunicipio) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    try {
+        const url = `https://sidra.ibge.gov.br/territorio#/N102/IN%20N6%20${codigoMunicipio}`;
+        await page.goto(url);
+
+        await page.waitForSelector('.lv-data');
+
+        const nomesDasCidades = await page.evaluate(() => {
+            const nomes = [];
+            document.querySelectorAll('.lv-data').forEach(element => {
+                const texto = element.textContent.trim();
+                if (texto && texto !== "Codigo" && texto !== "Nome") {
+                    const splitIndex = texto.indexOf(" - ");
+                    if (splitIndex !== -1) {
+                        const nomeBairro = texto.substring(0, splitIndex);
+                        nomes.push(nomeBairro);
+                    }
+                }
+            });
+            return nomes;
+        });
+
+        return nomesDasCidades;
+    } catch (error) {
+        throw new Error('Ocorreu um erro ao extrair os bairros:', error);
+    } finally {
+        await browser.close();
+    }
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
